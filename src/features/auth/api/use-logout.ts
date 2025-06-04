b9b6a1d5b52e59c30b0780557f14f2acd275d626
JonathanDCarvalho <168686459+JonathanDCarvalho@users.noreply.github.com>
2025-06-04 13:43:23 -0300

    5:38

--- src/features/auth/api/use-logout.ts
+++ src/features/auth/api/use-logout.ts
@@ -1,5 +1,6 @@
-import { useRouter } from "next/navigation";
+import { toast } from "sonner";
 import { InferResponseType } from "hono";
+import { useRouter } from "next/navigation";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 
 import { client } from "@/lib/rpc";
@@ -13,12 +14,21 @@ export const useLogout = () => {
 	const mutation = useMutation<ResponseType, Error>({
 		mutationFn: async () => {
 			const response = await client.api.auth.logout.$post();
+
+			if(!response.ok) {
+				throw new Error("Failed to logout");
+			}
 			return await response.json();
 		},
 		onSuccess: () => {
+			toast.success("Logged out");
 			router.refresh();
 			queryClient.invalidateQueries({ queryKey: ["current"] });
+			queryClient.invalidateQueries({ queryKey: ["workspaces"] });
 		},
+		onError: () => {
+			toast.error("Failed to log out");
+		}
 	});
 
 	return mutation;
--- src/features/workspaces/server/route.ts
+++ src/features/workspaces/server/route.ts
@@ -1,20 +1,66 @@
-import { zValidator } from "@hono/zod-validator";
 import { Hono } from "hono";
-import { createWorkspaceSchema } from "../schemas";
-import { DATABASE_ID, WORKSPACES_ID } from "@/config";
+import { ID, Query } from "node-appwrite";
+import { zValidator } from "@hono/zod-validator";
+import { MemberRole } from "@/features/members/types";
+import { generateInviteCode } from "@/lib/utils";
 import { sessionMiddleware } from "@/lib/session-middleware";
-import { ID } from "node-appwrite";
+import { DATABASE_ID, IMAGES_BUCKET_ID, MEMBERS_ID, WORKSPACES_ID } from "@/config";
+import { createWorkspaceSchema } from "../schemas";
 
 const app = new Hono()
+  .get("/", sessionMiddleware, async (c) => {
+    const user = c.get("user");
+    const databases = c.get("databases");
+
+    const members = await databases.listDocuments(
+      DATABASE_ID,
+      MEMBERS_ID,
+      [Query.equal("userId", user.$id)]
+    );
+
+    if (members.total === 0) {
+      return c.json({ data: { documents: [], total: 0 } });
+    }
+
+    const workspaceIds = members.documents.map((member) => member.workspaceId);
+
+    const workspaces = await databases.listDocuments(
+      DATABASE_ID,
+      WORKSPACES_ID,
+      [
+        Query.orderDesc("$createdAt"),
+        Query.contains("$id", workspaceIds)
+      ],
+    );
+    return c.json({ data: workspaces });
+  })
   .post(
     "/",
-    zValidator("json", createWorkspaceSchema),
+    zValidator("form", createWorkspaceSchema),
     sessionMiddleware,
     async (c) => {
       const databases = c.get("databases");
+      const storage = c.get("storage");
       const user = c.get("user");
 
-      const { name } = c.req.valid("json");
+      const { name, image } = c.req.valid("form");
+
+      let uploadedImageUrl: string | undefined;
+
+      if(image instanceof File) {
+        const file = await storage.createFile(
+          IMAGES_BUCKET_ID,
+          ID.unique(),
+          image,
+        );
+
+        const arrayBuffer = await storage.getFilePreview(
+          IMAGES_BUCKET_ID,
+          file.$id,
+        );
+
+        uploadedImageUrl = `data:image/png;base64,${Buffer.from(arrayBuffer).toString("base64")}`;
+      }
 
       const workspace = await databases.createDocument(
         DATABASE_ID,
@@ -22,8 +68,23 @@ const app = new Hono()
         ID.unique(),
         {
           name,
+          userId: user.$id,
+          imageUrl: uploadedImageUrl,
+          inviteCode: generateInviteCode(10),
         },
       );
+
+      await databases.createDocument(
+        DATABASE_ID,
+        MEMBERS_ID,
+        ID.unique(),
+        {
+          userId: user.$id,
+          workspaceId: workspace.$id,
+          role: MemberRole.ADMIN,
+        },
+      );
+
       return c.json({ data: workspace });
     }
   );
