c55e11e33a7d2b416889b80b25c373bed35c017b
JonathanDCarvalho <168686459+JonathanDCarvalho@users.noreply.github.com>
2025-07-04 13:58:14 -0300

    8:15

--- src/features/members/server/route.ts
+++ src/features/members/server/route.ts
@@ -3,6 +3,9 @@ import { Hono } from "hono";
 import { zValidator } from "@hono/zod-validator";
 import { createAdminClient } from "@/lib/appwrite";
 import { sessionMiddleware } from "@/lib/session-middleware";
+import { getMember } from "../utils";
+import { DATABASE_ID, MEMBERS_ID } from "@/config";
+import { Query } from "node-appwrite";
 
 const app = new Hono()
   .get(
@@ -13,6 +16,29 @@ const app = new Hono()
       const { users } = await createAdminClient();
       const databases = c.get("databases");
       const user = c.get("user")
+      const { workspaceId } = c.req.valid("query")
+
+      const member = await getMember({
+        databases,
+        workspaceId,
+        userId: user.$id,
+      });
+
+      if(!member) {
+        return c.json({ error: "Unauthorized" }, 401);
+      }
+
+      const members = await databases.listDocuments(
+        DATABASE_ID,
+        MEMBERS_ID,
+        [Query.equal("workspaceId", workspaceId)]
+      );
+
+      const populatedMembers = await Promise.all(
+        members.documents.map(async (member) => {
+          const user = await users.get(member.userId)
+        })
+      )
     }
   )
 
